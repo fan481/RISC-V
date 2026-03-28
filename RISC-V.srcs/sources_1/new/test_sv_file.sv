6a18f5f88db3fcedb414ee799b1ff0e85fb8b137
fan481 <296609502+fan481@users.noreply.github.com>
2026-03-28 09:13:00 -0400

    init

---
+++ RISC-V.srcs/sources_1/new/test_sv_file.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/28/2026 08:52:13 AM
+// Design Name: 
+// Module Name: test_sv_file
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_sv_file(
+    input [31:0] in,
+    output [31:0] out
+    );
+endmodule
